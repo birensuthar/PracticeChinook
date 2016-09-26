d55101b3ae56cc969fecc8f46527d7a12efe601e
Biren Suthar <bsuthar2@nait.ca>
2016-09-26 16:47:51 -0600

    Setup of Album and Artist Entities

---
+++ ChinookSample/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+
+    //Point to the sql table that this file maps
+    [Table("Albums")]
+
+    public class Album
+    {
+        //Key notations is optional if the sql pkey ends in id.
+        //Required if default entity is NOT Identity
+        //Required if pkey is compound.
+
+        //Properties can be fully implemented or auto implemented.
+        //Property names should use SQL attributes name
+        //Properties should be in the same order as sql attributes for ease of maintenance.
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+
+        //Navigation properties for use by linq.
+        //These properties will be of type virutal
+        //There are 2 types of navigation properties. 
+        //Properties that point to "children" use ICollection<T>
+        //Properties that point to "Parent" use the Parent Name as the datatpye
+        public virtual ICollection<Track> Tracks { get; set; }
+        public virtual Artist Artists { get; set; }
+    }
+}
---
+++ ChinookSample/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+
+    //Point to the sql table that this file maps
+    [Table("Artists")]
+
+    public class Artist
+    {
+        //Key notations is optional if the sql pkay ends in id.
+        //Required if default entity is NOT Identity
+        //Required if pkey is compound.
+
+        //Properties can be fully implemented or auto implemented.
+        //Property names should use SQL attributes name
+        //Properties should be in the same order as sql attributes for ease of maintenance.
+        [Key]
+        public int ArtistId { get; set; }
+
+        public string Name { get; set; }
+
+        //Navigation properties for use by linq.
+        //These properties will be of type virutal
+        //There are 2 types of navigation properties. 
+        //Properties that point to "children" use ICollection<T>
+        //Properties that point to "Parent" use the Parent Name as the datatpye
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
