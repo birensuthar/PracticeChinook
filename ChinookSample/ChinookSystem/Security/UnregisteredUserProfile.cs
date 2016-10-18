e6fe9c419db16642c1fe92dc0beb860620e5f1b3
Biren Suthar <bsuthar2@studentmail.nait.ca>
2016-10-18 16:08:20 -0600

    Set up of profile data set to be used to carry webpage data

---
+++ ChinookSample/ChinookSystem/Security/UnregisteredUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnRegisteredUserType { Undefined, Employee, Customer}
+    public class UnRegisteredUserProfile
+    {
+        public string UserId { get; set; } //Generated
+        public string UserName { get; set; } //Collected
+        public string Email { get; set; } //Collected
+        public string FirstName { get; set; } //Comes from the User Table
+        public string LastName { get; set; } //Comes from the User Table
+        public UnRegisteredUserType UserType { get; set; }
+    }
+}
